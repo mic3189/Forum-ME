0013779596886b87ed4921100243541621a1037f
Michael Angelo Alo <maalo@vjsol.jp>
2014-07-23 13:40:40 -0700

    fixing illegal offset type in User.php

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,4 +1,5 @@
 <?php
+App::uses('AuthComponent', 'Controller/Component');
 App::uses('AppModel', 'Model');
 
 class User extends AppModel
@@ -67,12 +68,7 @@ class User extends AppModel
 
 	public function beforeSave($options = array())
 	{
-		// hash our password
-		if (isset($this->data[$this->alias]['password'])) 
-		{
-			$this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->data]['password']);
-		}
-
-		return parent::beforeSave($options);
-	}
+    $this->data['User']['password'] = AuthComponent::password($this->data['User']['password']);
+    return true;
+  }
 }
